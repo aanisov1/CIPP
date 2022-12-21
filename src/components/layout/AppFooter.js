1857ffdf6be06aaa4b38651d61ec06cbeb064076
aanisov1 <a.anisov@nehgroup.com>
2022-12-21 14:29:07 +0100

    Hide footer -> return null

--- src/components/layout/AppFooter.js
+++ src/components/layout/AppFooter.js
@@ -5,25 +5,7 @@ import huntressLogo from 'src/assets/images/huntress_teal.png'
 import dattoLogo from 'src/assets/images/datto.png'
 
 const AppFooter = () => {
-  return (
-    <CFooter className="d-flex justify-content-between align-items-center">
-      <div className="sponsors">
-        <p>
-          This application is sponsored by{' '}
-          <CLink href="https://www.huntress.com/">
-            <CImage src={huntressLogo} alt="Huntress" />
-          </CLink>{' '}
-          &
-          <CLink href="https://datto.com/">
-            <CImage src={dattoLogo} alt="Datto" />
-          </CLink>
-        </p>
-      </div>
-      <nav className="footer-nav">
-        <Link to="/license">License</Link>
-      </nav>
-    </CFooter>
-  )
+  return null
 }
 
 export default React.memo(AppFooter)
